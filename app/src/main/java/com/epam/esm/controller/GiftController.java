3831fcce1ec73ffc7bbfb6d235f67d39fd7736ea
VadimKominch <kominch.vadim@yandex.ru>
2020-10-29 18:11:55 +0300

    feat: add some basic http method for rest app

--- app/src/main/java/com/epam/esm/controller/GiftController.java
+++ app/src/main/java/com/epam/esm/controller/GiftController.java
@@ -3,13 +3,18 @@ package com.epam.esm.controller;
 import com.epam.esm.entity.GiftSertificate;
 import com.epam.esm.service.GiftService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.List;
-
+/**
+ *
+ * Rest Controller for basic crud operations and operations with list of data.
+ * Use service as way to connect with database
+ * @author Vadim Kominch
+ *
+ * */
 @RestController
 public class GiftController {
 
@@ -23,13 +28,17 @@ public class GiftController {
     public String getString() {
         return "Hello,world";
     }
-
+/**
+ * Get method for reading one entity if its present. Receive id as integer in url path
+ * and find entity in database.
+ * */
     @GetMapping(value = "/{id}")
     public GiftSertificate getGiftById(@PathVariable int id) {
         return giftService.getById(id);
     }
 /**
- * Get method for requesting all existing Sertificates. Use gift service for reciveing info from database
+ * Get method for requesting all existing certificates. Use gift service
+ * for receiving info from database.
  * @see GiftService
  * */
     @GetMapping(value = "/all")
@@ -45,6 +54,28 @@ public class GiftController {
     @ResponseStatus(HttpStatus.CREATED)
     public String saveEntity(@RequestBody GiftSertificate giftSertificate) {
         giftService.save(giftSertificate);
+        return "OK";
+    }
+
+    /**
+     * Post method for storing list of data from json format. Entity saved in database. Receive
+     * list in JSON.
+     * Return 201 status code if request is successful.
+     * */
+    @PostMapping(value = "/add_all")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String addAll(@RequestBody List<GiftSertificate> certificates) {
+        certificates.forEach(el->giftService.save(el));
+        return "OK";
+    }
+    /**
+     * Delete method for deleting entity by id from database. Receive id as integer
+     * and find entity with id in database.
+     * */
+    @DeleteMapping("delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String deleteOne(@PathVariable int id) {
+        giftService.delete(id);
         return "OK";
     }
 }
